f53fb1d2e5a0b853f53646fc36d2cd4f70160b59
busratok <busratokuc@gmail.com>
2026-03-27 00:04:25 +0000

    style: add app.module.css and update index.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import "./App.css";
+import styles from "./App.module.css";
 import FeedBackForm from "./components/FeedBackForm/FeedBackForm";
 
 function App() {
   return (
-    <main className="app-container">
+    <main>
       <header>
-        <h1>HDRUK Feedback</h1>
-        <p>Junior Frontend Developer Task 1</p>
+        <h1 className={styles.headerTitle}>HDRUK Feedback</h1>
+        <p className={styles.headerSubTitle}>
+          Junior Frontend Developer Task 1
+        </p>
       </header>
       <section>
-        <h2>Feedback Form</h2>
+        <h2 className={styles.sectionTitle}>Feedback Form</h2>
         <FeedBackForm />
       </section>
     </main>
